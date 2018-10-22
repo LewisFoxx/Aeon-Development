99d01bd3116ef516c0dceccac9cb95196bf252a3
JoshH-H <j.hallhems@gmail.com>
2018-10-22 22:06:56 +0100

    Forest and Menu changes

---
+++ Aeon/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void MENU_ACTION_GoToPage(string sceneName)
+	{
+		Application.LoadLevel(sceneName);
+	}
+}
