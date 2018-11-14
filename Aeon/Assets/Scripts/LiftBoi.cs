e7e034810d6a7617f2520888d7568a602c9adf0f
JoshH-H <j.hallhems@gmail.com>
2018-11-14 20:25:16 +0000

    Lift Changes

---
+++ Aeon/Assets/Scripts/LiftBoi.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class LiftBoi : MonoBehaviour {
+
+      //public bool animation_bool;
+          
+		void Update()
+					 {
+            if(Input.GetKeyDown("b"))
+			//if(animation_bool == false)
+                     {
+                GetComponent<Animation>().Play("Lower");
+                     }
+
+                     {
+                //animation_bool = true;
+                     }
+ 
+ 
+ 
+                }
+}
