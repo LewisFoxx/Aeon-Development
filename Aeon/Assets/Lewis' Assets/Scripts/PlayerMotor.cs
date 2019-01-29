217380850722e4f140445044de38c35ff3ae9d6b
Lewis Fox <lewispaulfox@gmail.com>
2019-01-29 11:57:36 +0000

    Arm Rigging Begun

---
+++ Aeon/Assets/Lewis' Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    float moveSpeed = 4;
+    float gravity = 6;
+
+    Vector3 moveDirection;
+
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController> ();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Move()
+    {
+        float moveX = Input.GetAxis ("Horizontal");
+        float moveZ = Input.GetAxis ("Vertical");
+
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(moveX, 0, moveZ);
+        }
+
+    }
+}
