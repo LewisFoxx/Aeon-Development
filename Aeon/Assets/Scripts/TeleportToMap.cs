8ec8fa7779bc812cc658ed911159b17ddc30159b
JoshH-H <j.hallhems@gmail.com>
2019-01-29 11:55:43 +0000

    Map Region

---
+++ Aeon/Assets/Scripts/TeleportToMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportToMap : MonoBehaviour {
+
+public GameObject MapText;
+//public GameObject AttachedObject;
+
+
+void OnTriggerEnter (Collider other){
+if (other.gameObject.tag == "Player")
+MapText.SetActive (true);
+StartCoroutine ("ExplainOff");
+}
+
+IEnumerator ExplainOff() {
+yield return new WaitForSeconds (10);
+MapText.SetActive (false);
+}
+}
