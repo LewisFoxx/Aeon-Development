32b7b1b83227a0344b3d1aadceabf839025458e0
Lewis <43741704+LewisFoxx@users.noreply.github.com>
2019-03-06 21:10:49 +0000

    Aura is now working.

--- Aeon/Assets/Aura/Structs/TransformParameters.cs
+++ Aeon/Assets/Aura/Structs/TransformParameters.cs
@@ -1,23 +1,21 @@
-﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///                                                                                                                                                             ///
-///     MIT License                                                                                                                                             ///
-///                                                                                                                                                             ///
-///     Copyright (c) 2016 Raphaël Ernaelsten (@RaphErnaelsten)                                                                                                 ///
-///                                                                                                                                                             ///
-///     Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),      ///
-///     to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute,                  ///
-///     and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:              ///
-///                                                                                                                                                             ///
-///     The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.                          ///
-///                                                                                                                                                             ///
-///     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     ///
-///     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER      ///
-///     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS    ///
-///     IN THE SOFTWARE.                                                                                                                                        ///
-///                                                                                                                                                             ///
-///     PLEASE CONSIDER CREDITING AURA IN YOUR PROJECTS. IF RELEVANT, USE THE UNMODIFIED LOGO PROVIDED IN THE "LICENSE" FOLDER.                                 ///
-///                                                                                                                                                             ///
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+﻿/***************************************************************************
+*                                                                          *
+*  Copyright (c) Raphaël Ernaelsten (@RaphErnaelsten)                      *
+*  All Rights Reserved.                                                    *
+*                                                                          *
+*  NOTICE: Although Aura (or Aura 1) is still a free project, it is not    *
+*          open-source nor in the public domain anymore.                   *
+*          Aura is now governed by the End Used License Agreement of       *
+*          the Asset Store of Unity Technologies.                          *
+*                                                                          * 
+*  All information contained herein is, and remains the property of        *
+*  Raphaël Ernaelsten.                                                     *
+*  The intellectual and technical concepts contained herein are            *
+*  proprietary to Raphaël Ernaelsten and are protected by copyright laws.  *
+*  Dissemination of this information or reproduction of this material      *
+*  is strictly forbidden.                                                  *
+*                                                                          *
+***************************************************************************/
 
 using System;
 using UnityEngine;
