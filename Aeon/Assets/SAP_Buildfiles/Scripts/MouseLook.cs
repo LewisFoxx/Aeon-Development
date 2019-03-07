7c27ab00a364e5551250ddb39430624c923676eb
JoshCollege <71898@havering-sfc.ac.uk>
2019-03-07 16:22:42 +0000

    New Changes

---
+++ Aeon/Assets/SAP_Buildfiles/Scripts/MouseLook.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+	private GameObject player;
+	private float minClamp = -45;
+	private float maxClamp = 45;
+	[HideInInspector]
+	public Vector2 rotation;
+	private Vector2 currentLookRot;
+	private Vector2 rotationV = new Vector2 (0, 0);
+
+	public float lookSensistvity = 2;
+	public float lookSmoothDamp = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		//Access the player GameObject
+		player = transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Player input from mouse
+		rotation.y += Input.GetAxis("Mouse Y") * lookSensistvity;
+		//Limit ability to look up and down
+		rotation.y = Mathf.Clamp(rotation.y, minClamp, maxClamp);
+		//Rotate the character around based on the mouse x position
+		player.transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Mouse X") * lookSensistvity);
+		//Smooth the current y rotation for looking up and down
+		currentLookRot.y = Mathf.SmoothDamp(currentLookRot.y, rotation.y, ref rotationV.y, lookSmoothDamp);
+		//Update the camera x rotation based on values generated.
+		transform.localEulerAngles = new Vector3(-currentLookRot.y,0,0);
+	}
+}
---
+++ Aeon/Assets/SAP_Buildfiles/Scripts/PlayerDamage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDamage : MonoBehaviour {
+
+	//Use this to refrence the text in the canvas
+	public Text healthPanel;
+	//Sets the default health to 100
+	public int health = 100;
+
+	// Use this for initialization
+	private void Start () {
+		//Sets the health etxt at the start, we pass 0 as we don't want to remove the health.
+		ApplyDamage(0);
+	}
+	
+	// Update is called once per frame
+	void ApplyDamage (int damage) {
+		//Checks we have attached a health panel and our health is greater than 0
+		if (healthPanel != null && health > 0) {
+			//Stores the current health and subtracts the damage value
+			health = health - damage;
+			//Sets the text on our panel
+			healthPanel.text = health.ToString ();
+
+		}
+	}
+}
