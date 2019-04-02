d0903dcb89083187d215307cc0b5a02fda037821
JoshH-H <j.hallhems@gmail.com>
2019-04-02 11:38:07 +0100

    GunSounds

--- Aeon/Assets/Scripts/Scripts/ActivateProjectile.cs
+++ Aeon/Assets/Scripts/Scripts/ActivateProjectile.cs
@@ -10,7 +10,7 @@ public class ActivateProjectile : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
-			//playerShot.Play ();
+			playerShot.Play ();
 			var clone = Instantiate (projectile, gameObject.transform.position, gameObject.transform.rotation);
 			//Destroy after 2 seconds to stop clutter
 			Destroy (clone, 5.0f);
