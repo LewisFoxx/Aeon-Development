020276f9a1e95fce73cd505780d4ce5c15a34172
JoshH-H <j.hallhems@gmail.com>
2019-05-01 13:52:57 +0100

    Menu Enhancements and Flashlight Partially Inplemented

--- Aeon/Assets/Tree dome/FlashLight.cs
+++ Aeon/Assets/Tree dome/FlashLight.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FlashLight : MonoBehaviour {
-
-
-
-    // Update is called once per frame
-    void Update ()
-    {
-        if (Input.GetKeyDown(KeyCode.F)) ;
-           // FlashLight.SetActive(true);
-
-    }
+		
+	public Light thelight;
+	public bool on = true;
+	
+	// Use this for initialization
+	void Start () {
+		on = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			if(on = true)
+			{
+				thelight.enabled = false;
+				on = false;
+			} 
+			else if(on == false)
+			{
+				thelight.enabled = true;
+				on = true;
+			}
+	}
+}
 }
