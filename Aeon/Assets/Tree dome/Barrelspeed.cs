a7f8f985342e3ebfe79ee301c06c4492fabd38b2
JoshH-H <j.hallhems@gmail.com>
2019-05-04 23:47:53 +0100

    Forest Stuff

---
+++ Aeon/Assets/Tree dome/Barrelspeed.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrelspeed : MonoBehaviour 
+{
+	Animation anim;
+	private float animSpeed = 0.02f;
+	
+    void Start()
+	{
+	anim = GetComponent<Animation>();
+	}
+	
+	void Update()
+	{
+	anim["barrel"].speed = animSpeed;
+	}
+}
