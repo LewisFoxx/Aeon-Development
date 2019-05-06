273eb7c2d3019778955f8d7f510e5773d688592e
JoshH-H <j.hallhems@gmail.com>
2019-05-06 13:36:42 +0100

    Create CollectCard.cs

---
+++ Aeon/Assets/CollectCard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCard : MonoBehaviour {
+
+	public int RotateSpeed;
+	public AudioSource CollectSound;
+	public GameObject Card;
+	
+
+	void Update () {
+		RotateSpeed = 2;
+		transform.Rotate (0, RotateSpeed, 0, Space.World);
+	}
+
+
+	void OnTriggerEnter () {
+		CollectSound.Play ();
+		Card.SetActive (false);
+	}
+}
