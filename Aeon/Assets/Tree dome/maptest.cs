00f414971a2be72160de8eedb10f3796c9497088
JoshH-H <j.hallhems@gmail.com>
2019-05-08 22:40:34 +0100

    Map WIP

--- Aeon/Assets/Tree dome/maptest.cs
+++ Aeon/Assets/Tree dome/maptest.cs
@@ -6,21 +6,33 @@ public class maptest : MonoBehaviour
 {
 
     public GameObject MAP;
+	public GameObject Weapon;
+	public GameObject Reticle;
+	public GameObject Compass;
     //public GameObject AttachedObject;
 
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")//&& (Input.GetKeyDown("m")))
-            MAP.SetActive(true);
-		Cursor.visible = true;
-        StartCoroutine("MapOff");
+		Cursor.visible=true; 
+        MAP.SetActive (true);
+		Weapon.SetActive (false);
+		Reticle.SetActive (false);
+		Compass.SetActive (false);
+		//Cursor.lockState = CursorLockMode.None;
+		StartCoroutine("MapOff");
     }
 
     IEnumerator MapOff()
     {
         yield return new WaitForSeconds(10);
         //CollectExplain.SetActive (true);
+		Weapon.SetActive (true);
+		Reticle.SetActive (true);
+		Compass.SetActive (true);
+		Cursor.visible = false;
+		//Cursor.lockState = CursorLockMode.Locked;
         MAP.SetActive(false);
     }
 }
