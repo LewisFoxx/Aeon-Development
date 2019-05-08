a9b8371fcc4fbdfafe827969ff8011759d8b5022
JoshH-H <j.hallhems@gmail.com>
2019-05-08 17:43:33 +0100

    Boss ACTUALLY WORKING
    Shoot all weakpoints to show green

---
+++ Aeon/Assets/Tree dome/Boss Testing/Bosskill.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bosskill : MonoBehaviour {
+
+	public GameObject Card;
+	public GameObject Explosion;
+	public GameObject Pannel2;
+
+
+	void OnTriggerEnter () 
+	{
+		Card.SetActive (false);
+		Explosion.SetActive (true);
+		Pannel2.SetActive (true);
+		
+	}
+}
