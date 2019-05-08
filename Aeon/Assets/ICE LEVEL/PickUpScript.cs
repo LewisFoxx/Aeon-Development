90ff627a169d2571ec500f34f7f9595ebcd7e5ce
JoshH-H <j.hallhems@gmail.com>
2019-05-08 13:27:16 +0100

    Revert "Merge branch 'master' of https://github.com/LewisFoxx/Aeon-Development"
    This reverts commit bf3496784776f5454177221736e7bcf3d21c74c2, reversing
    changes made to 1f41b898311b5eb871407f9d469a000f7f337188.

--- Aeon/Assets/ICE LEVEL/PickUpScript.cs
+++ Aeon/Assets/ICE LEVEL/PickUpScript.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickUpScript : MonoBehaviour
-{
+public class PickUpScript : MonoBehaviour{
 
-    public GameObject Card;
+    public Transform theDest;
 
-
-
-
-
-    void OnTriggerEnter()
+    void OnMouseDown()
     {
-        Card.SetActive(false);
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = theDest.position;
+        this.transform.parent = GameObject.Find("Destination").transform;
     }
+    void OnMouseUp()
+    {
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<BoxCollider>().enabled = true;
+    }
+  
 }
