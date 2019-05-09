36491eb5e06e4bc5e93b8a0022c42d68d775c294
Lewis Fox <lewispaulfox@gmail.com>
2019-05-09 16:23:47 +0100

    Desert Level Changes

---
+++ Aeon/Assets/Lewis' Assets/Scripts/LightTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTrigger : MonoBehaviour {
+
+    public GameObject Light;
+
+
+    void OnTriggerEnter ()
+    {
+        Light.SetActive (true);
+    }
+		
+}
