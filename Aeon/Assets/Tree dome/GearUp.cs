98cadb86eb9b4b70370c400fda861a1ff77705b6
JoshH-H <j.hallhems@gmail.com>
2019-05-12 12:23:37 +0100

    Map,Forest changes

---
+++ Aeon/Assets/Tree dome/GearUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearUp : MonoBehaviour {
+
+	public GameObject Case;
+	public GameObject Weapon;
+	public GameObject Reticle;
+	public GameObject Compass;
+	// GameObject HUD;
+	public AudioSource ReadyUp;
+	
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")//&& (Input.GetKeyDown("m")))
+			Case.SetActive (false);
+			Weapon.SetActive (true);
+			Reticle.SetActive (true);
+			Compass.SetActive (true);
+			//HUD.SetActive (true);
+			ReadyUp.Play();
+	}
+}
